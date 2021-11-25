945e732d00bd71fefcd8515cf5a01f77c1d7307a
Lance Hebert <LSUHEBERT@gmail.com>
2021-11-24 16:11:47 -0800

    commit tree e636d826624f473e0f6aaaf2196c2bb943dff2f1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+function superbowlWin(array)
+{
+    const finder = array.find(function(value){
+        if(value.result === "W")
+        {
+           return value;     
+        }
+    }) 
+       
+    if(finder !== undefined)
+    {
+        return finder.year;
+    }
+       
+}
+
+
+
